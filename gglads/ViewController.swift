3082cacb5b03862e5d90a47b19883dd31ef1e573
Паша <Pasha@macbook-air-pasa.local>
2017-02-04 13:09:09 +0300

    Initial Commit

---
+++ gglads/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gglads
+//
+//  Created by Паша on 04.02.17.
+//  Copyright © 2017 Паша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
